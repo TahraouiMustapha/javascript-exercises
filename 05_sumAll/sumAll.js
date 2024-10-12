40aad5c990731e5855a0ae0c24733f4812acd6db
mustapha <moustaphaoussama1818@gmail.com>
2024-10-12 09:16:46 +0100

    feat: complete the exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    if(startNum < 0 || endNum < 0) return 'ERROR';
+    else if(!Number.isInteger(startNum) 
+        || !Number.isInteger(endNum)) return 'ERROR';
 
+    let sum = 0;
+    let minNum = startNum < endNum ? startNum : endNum ;
+    let maxNum = startNum > endNum ? startNum : endNum ;
+    for(let i = minNum; i <= maxNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
