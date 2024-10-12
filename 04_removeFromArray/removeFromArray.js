d014e94fd9750e669585f729a241699acc177e45
mustapha <moustaphaoussama1818@gmail.com>
2024-10-12 08:43:24 +0100

    feat: complete the exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...otherArguments) {
+    for (const arg of otherArguments) {
+        while(array.includes(arg)) {
+            let indexOfArg = array.indexOf(arg);
+            array.splice(indexOfArg, 1);
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
