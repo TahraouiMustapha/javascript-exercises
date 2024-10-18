4d76579c304ec51471e9d0b4c6be0fc9a420d826
mustapha <moustaphaoussama1818@gmail.com>
2024-10-18 18:31:48 +0100

    feat : complete the exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b; 
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, curr) => total + curr , 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, curr) => total * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if(number === 0) return 1;
+  else return number * factorial(number - 1);
 };
 
 // Do not edit below this line
