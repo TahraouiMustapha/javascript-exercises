6709396a3a7d844c83c7133009a419213e85b4a8
mustapha <moustaphaoussama1818@gmail.com>
2024-10-18 20:00:24 +0100

    feat : complete the exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const myArray = string.replace(/[\s,.!]+/g, '').split('');
+    return myArray.join().toLowerCase() === myArray.reverse().join().toLowerCase();
 };
 
 // Do not edit below this line
