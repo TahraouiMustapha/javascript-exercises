8bd32a24db11c83e129d4045f692bfb1c048b88c
mustapha <moustaphaoussama1818@gmail.com>
2024-10-18 20:24:40 +0100

    feat : complete the exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const number = Number(num); 
+    if(number < 0) return "OOPS";
+    const fibonacciSerie = [0, 1];
+    for(let i = 2; i <= number; i++) {
+        fibonacciSerie[i] = fibonacciSerie[i - 1] + fibonacciSerie[i - 2];
+    }
+    return fibonacciSerie[number];
 };
 
 // Do not edit below this line
