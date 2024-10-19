c439db43a85fec24887667cc1fc9a25bef1911a2
mustapha <moustaphaoussama1818@gmail.com>
2024-10-19 18:09:34 +0100

    feat : complete the exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+function getAge (birth, death) {
+    if(!death) {// if death != undefined, null , 0, NaN..
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(people) {
+   return people.reduce((oldest, current) => {
+    const oldestAge = getAge(
+        oldest.yearOfBirth, 
+        oldest.yearOfDeath
+    );
+    const currentAge = getAge(
+        current.yearOfBirth, 
+        current.yearOfDeath
+    );
+
+    return currentAge < oldestAge ? oldest: current;
+   })
 };
 
 // Do not edit below this line
